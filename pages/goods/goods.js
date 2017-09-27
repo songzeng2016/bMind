62df10f879d1dd132258d60e7365ce614c046697
wuyingyi <wuyingyi@99114.com>
2017-09-27 15:42:06 +0800

    收藏

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -82,6 +82,16 @@ Page({
   },
 
 
+//收藏切换
+  collectCut:function(e){
+    console.log(e)
+    var index = e.currentTarget.dataset.index;
+    var list = this.data.list
+    console.log(list)
+    list[index].praise = !(list[index].praise || false);
+    this.setData({ list })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
