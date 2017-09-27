af9d3cb3b4a668228af7e37ce9ff102beb09be50
songzeng2016 <songzeng62@gmail.com>
2017-09-27 10:13:32 +0800

    获取物品页面信息

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1,4 +1,8 @@
 // pages/goods/goods.js
+const app = getApp()
+const { wc } = app
+const { host, data, isSuccess, success } = wc
+
 Page({
 
   /**
@@ -10,7 +14,7 @@ Page({
       "../../image/2.jpg"
     ],
 
-    list:[
+    list: [
       {
         placeSortSelect: "全部分类",
         placePurSortOpen: true,
@@ -38,7 +42,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const that = this
+
+    // banner
+    let bData = {
+      Action: 'GetAd',
+      ClassID: 3
+    }
+    wc.get(bData, (json) => {
+      if (json[isSuccess] === success) {
+        that.setData({
+          imgUrls: json[data]
+        })
+      }
+    })
+
+    // list
+    let getData = {
+      Action: 'GetProductList',
+      KeyWords: '',
+      ClassID: 0,
+      SortType: 0,
+      TimeType: 0,
+      pageSize: 10,
+      pageIndex: 1
+    }
+
+    wc.get(getData, (json) => {
+      if (json[isSuccess] === success) {
+        that.setData({
+          goodList: json[data]
+        })
+      }
+    })
   },
 
   // 产地展开收起
@@ -48,7 +84,7 @@ Page({
     var status = this.data.list[index].placePurSortOpen
     var list = this.data.list
     list[index].placePurSortOpen = !status
-    this.setData({list});
+    this.setData({ list });
   },
   //产地筛选条件  change value
   placeSortChangeFn: function (e) {
@@ -62,48 +98,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
